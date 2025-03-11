be38dc1f40f43c6373055c7ff06101fffe0b3889
manalankm <>
2025-03-11 18:05:47 +1100

    feature: playlist refresher function

--- src/functions/playlistRefresher.ts
+++ src/functions/playlistRefresher.ts
@@ -1,14 +1,21 @@
-import { app, InvocationContext, Timer } from "@azure/functions";
+import { app, InvocationContext, Timer } from '@azure/functions';
+import { PlaylistRefresher } from '../utils/PlaylistRefresh';
+export async function playlistRefresher(
+  myTimer: Timer,
+  context: InvocationContext,
+): Promise<void> {
+  // context.log('Timer function processed request.');
+  if (myTimer.isPastDue) {
+    context.log('Function executed way too late bro');
+  } else {
+    const playlistRefresher = new PlaylistRefresher(process.env.REFRESH_TOKEN!);
 
-export async function playlistRefresher(myTimer: Timer, context: InvocationContext): Promise<void> {
-    // context.log('Timer function processed request.');
-    if(myTimer.isPastDue) { 
-        context.log('Function executed way too late bro')
-    }
-    console.log('Timer Function executed meow meow');
+    context.log('Refresh start!');
+    playlistRefresher.refreshPlaylist();
+  }
 }
 
 app.timer('playlistRefresher', {
-    schedule: '0 */1 * * * *',
-    handler: playlistRefresher
+  schedule: '0 */1 * * * *',
+  handler: playlistRefresher,
 });
